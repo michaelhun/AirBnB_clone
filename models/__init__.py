dfa54b275760d91967313f67abacafe730aa988e
michaelhun <mikhun12@gmail.com>
2023-05-15 01:43:19 -0700

    update

--- models/__init__.py
+++ models/__init__.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
 """__init__ magic method for models directory"""
-from .engine.file_storage import FileStorage
-from .base_model import BaseModel
-from .user import User
-from .review import Review
-from .city import City
-from .amenity import Amenity
-from .place import Place
-from .state import State
+from models.engine.file_storage import FileStorage
 
 
 storage = FileStorage()
